ae4ca6c52cc12a02b21fa5c2aae54b91198f798f
Lais <laisgolin@gmail.com>
2022-02-21 13:42:37 +0000

    new exercises

--- exercicios/ex001/script.js
+++ exercicios/ex001/script.js
@@ -2,18 +2,20 @@ function carregar() {
   var msg = document.querySelector("#msg");
   var img = document.querySelector("#img");
 
-  //var horaAtual = new Date().getHours();
-  var horaAtual = 19;
+  var horaAtual = new Date().getHours();
   msg.innerHTML = `Agora são ${horaAtual} horas.`;
 
   if (horaAtual < 12) {
     img.src = "./img/manha.png";
     document.body.style.background = "#AC9A8E";
+    msg.innerHTML = " Bom dia!";
   } else if (horaAtual < 18) {
     img.src = "./img/tarde.png";
     document.body.style.background = "#d89d6a";
+    msg.innerHTML += "<br>Boa tarde!";
   } else {
     img.src = "./img/noite.png";
     document.body.style.background = "#3b4149";
+    msg.innerHTML = " Boa noite!";
   }
 }
