bc9132feef5fd72a4e26de5da46346129b15293a
Lais <laisgolin@gmail.com>
2022-03-02 17:02:06 +0000

    new exercises

---
+++ moduloF/4.funcao.js
@@ -0,0 +1,14 @@
+function parimp(n) {
+  if (n % 2 == 0) {
+    return "par";
+  } else {
+    return "impar";
+  }
+} /*
+let res = parimp(11);
+console.log(res);
+*/
+console.log(parimp(12));
+console.log(parimp(223));
+console.log(parimp(21));
+console.log(parimp(54));
---
+++ moduloF/6.funcao.js
@@ -0,0 +1,7 @@
+// é possível jogar uma função dentro de uma variável
+
+let mul = function multiplica(n1) {
+  return n1 * 2;
+};
+
+console.log(mul(2));
